2bb3dffb06ab1c37e1a935b8ecccb18e260179ea
Dawdaborje <dawdaborjekujabi@gmail.com>
2025-10-07 13:28:48 +0000

    Starting from scratch..

--- src/lib/index.ts
+++
@@ -1,9 +0,0 @@
-// place files you want to import through the `$lib` alias in this folder.
-
-// Export theme utilities
-export { themeStore, themes, type Theme, type ColorMode, type ThemeConfig } from './stores/theme';
-
-// Export components
-export { default as ThemeSelector } from './components/ThemeSelector.svelte';
-export { default as ColorModeToggle } from './components/ColorModeToggle.svelte';
-export { default as LanguageSelector } from './components/LanguageSelector.svelte';
